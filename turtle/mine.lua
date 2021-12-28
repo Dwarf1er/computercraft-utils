c22ef0f7da44d7d56edcbd3ee22af499ad3556b6
Alexandre Lavoie <alexandre.lavoie00@gmail.com>
2021-12-27 20:04:12 -0500

    Trying to mine ore nearby.

--- turtle/mine.lua
+++ turtle/mine.lua
@@ -1,11 +1,6 @@
 function check_blocks()
     blocks = {}
 
-    local success, data = turtle.inspect()
-    if success then
-        blocks["Forward"] = data
-    end
-
     local success, data = turtle.inspectUp()
     if success then
         blocks["Up"] = data
@@ -14,6 +9,11 @@ function check_blocks()
     local success, data = turtle.inspectDown()
     if success then
         blocks["Down"] = data
+    end
+
+    local success, data = turtle.inspect()
+    if success then
+        blocks["Forward"] = data
     end
 
     turtle.turnLeft()
@@ -39,10 +39,40 @@ function check_blocks()
     return blocks
 end
 
+function mine_direction(direction)
+    if direction == "Up" then
+        turtle.digUp()
+    elseif direction == "Down" then
+        turtle.digDown()
+    elseif direction == "Forward" then
+        turtle.dig()
+    elseif direction == "Left" then
+        turtle.turnLeft()
+        turtle.dig()
+        turtle.turnRight()
+    elseif direction == "Right" then
+        turtle.turnRight()
+        turtle.dig()
+        turtle.turnLeft()
+    elseif direction == "Back" then
+        turtle.turnLeft()
+        turtle.turnLeft()
+        turtle.dig()
+        turtle.turnLeft()
+        turtle.turnLeft()
+    end
+end
+
 function mine_forward()
     blocks = check_blocks()
 
-    if blocks["Forward"] == nil then
-        turtle.forward()
+    for direction, data in pairs(blocks) do
+        if data["name"]:match("ore") then
+            mine_direction(direction)
+        end
     end
+
+    mine_direction("Forward")
+    
+    return turtle.forward()
 end
